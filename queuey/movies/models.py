7eaed0534e1e22aa8522cc59bb9c330a2a82519a
Kyle Chivers <chiverskyle@gmail.com>
2020-06-21 15:26:47 -0700

    added fixture for statuses

--- queuey/movies/models.py
+++ queuey/movies/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=500)
+    tmdb_id = models.IntegerField()
+    imdb_id = models.CharField(max_length=500)
+    movie_description = models.TextField()
+    poster_url = models.URLField()
+    release_date = models.DateField()
+    rating = models.FloatField()
+    # status =
