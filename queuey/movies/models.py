5d4094b468ca9983326ddfc49434beaa0748dccc
Kyle Chivers <chiverskyle@gmail.com>
2020-07-05 12:17:55 -0700

    created movie user model

--- queuey/movies/models.py
+++ queuey/movies/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from genres.models import Genre
 from platforms.models import Platform
 from statuses.models import Status, Priority
@@ -24,16 +25,23 @@ class Movie(models.Model):
     poster_url = models.CharField(max_length=500, null=True, blank=True)
     release_date = models.DateField(null=True, blank=True)
     runtime = models.IntegerField(null=True, blank=True)
-    rating = models.FloatField(null=True, blank=True)
     vote_average = models.FloatField(null=True, blank=True)
     vote_count = models.IntegerField(null=True, blank=True)
     genres = models.ManyToManyField(Genre, blank=True)
     collection = models.ManyToManyField(Collection, blank=True)
+
+    def __str__(self):
+        return self.title
+
+class MovieUser(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     platforms = models.ManyToManyField(Platform, blank=True)
     status = models.ForeignKey(Status, on_delete=models.SET_NULL, null=True, blank=True)
     watched_date = models.DateField(null=True, blank=True)
     priority = models.ForeignKey(Priority, on_delete=models.SET_NULL, null=True, blank=True)
+    rating = models.FloatField(null=True, blank=True)
     notes = models.TextField(null=True, blank=True)
 
     def __str__(self):
-        return self.title
+        return self.user, self.movie
