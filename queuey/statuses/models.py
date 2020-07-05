85783ab314419592d1b18787a2475734d4a52718
Kyle Chivers <chiverskyle@gmail.com>
2020-07-05 11:22:44 -0700

    added plural names

--- queuey/statuses/models.py
+++ queuey/statuses/models.py
@@ -5,5 +5,17 @@ from django.db import models
 class Status(models.Model):
     status = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name_plural = 'statuses'
+
+    def __str__(self):
+        return self.status
+
 class Priority(models.Model):
     priority = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = 'priorities'
+
+    def __str__(self):
+        return self.priority
